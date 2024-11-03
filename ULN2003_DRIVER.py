62dc10ff55365a66b14637fab9e23fd1dee4fd62
Asher Meachum <132758546+Asher-Meachum@users.noreply.github.com>
2024-11-03 08:50:46 -0600

    Update ULN2003_DRIVER.py
    Optimise clockwise function
    Add some input checking for delay input to functions clockwise and counterclockwise
    Remove superfluous function deinit

--- ULN2003_DRIVER.py
+++ ULN2003_DRIVER.py
@@ -8,48 +8,52 @@ class ULN2003:
         self.C = Pin(c, Pin.OUT)
         self.D = Pin(d, Pin.OUT)
 
+        # Used to cycle through pins in clockwise and counterclockwise functions
         self.pins = {
             0 : self.A,
             1 : self.B,
             2 : self.C,
             3 : self.D
+            4 : self.A
             }
-    def clockwise(self, steps, delay):
-        for s in range(steps):
-            i = 0
-            while i < 3:
-                self.pins[i].value(1)
-                self.pins[i+1].value(1)
-                sleep(delay)
-                self.pins[i].value(0)
-                self.pins[i+1].value(0)
-                i+=1
-                sleep(delay)
-            self.D.value(1)
-            self.A.value(1)
-            sleep(delay)
-            self.D.value(0)
-            self.A.value(0)
-            sleep(delay)
-    def counterclockwise(self, steps, delay):
-        for s in range(steps):
-            i = 3
-            self.D.value(1)
-            self.A.value(1)
-            sleep(delay)
-            self.D.value(0)
-            self.A.value(0)
-            while i > 0:
-                self.pins[i].value(1)
-                self.pins[i-1].value(1)
-                sleep(delay)
-                self.pins[i].value(0)
-                self.pins[i-1].value(0)
-                i-=1
-                sleep(delay)
     
-    def deinit(self):
-        i=0
-        while i <= 3:
-            self.pins[i].value(0)
-            i+=1
+    def clockwise(self, steps, delay):
+        if delay >= 0.002: # It won't move if it's below 0.002 (tested to 0.00199)
+            for s in range(steps):
+                i = 0
+                while i < 3:
+                    self.pins[i].value(1)
+                    self.pins[i+1].value(1)
+                    sleep(delay)
+                    self.pins[i].value(0)
+                    self.pins[i+1].value(0)
+                    i+=1
+                    sleep(delay)
+        else:
+            i = 0
+            while i < 4:
+                self.pins[i].value(0)
+                i+=1
+    
+    def counterclockwise(self, steps, delay):
+        if delay >= 0.002:
+            for s in range(steps):
+                i = 3
+                self.D.value(1)
+                self.A.value(1)
+                sleep(delay)
+                self.D.value(0)
+                self.A.value(0)
+                while i > 0:
+                    self.pins[i].value(1)
+                    self.pins[i-1].value(1)
+                    sleep(delay)
+                    self.pins[i].value(0)
+                    self.pins[i-1].value(0)
+                    i-=1
+                    sleep(delay)
+        else:
+            i = 0
+            while i < 4:
+                self.pins[i].value(0)
+                i+=1
