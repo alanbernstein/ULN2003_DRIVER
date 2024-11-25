8e7f61dad64882bb2eef640ea8fadbda862c0c30
Asher Meachum <132758546+Asher-Meachum@users.noreply.github.com>
2024-11-24 18:43:02 -0600

    Update ULN2003_DRIVER.py
    Significant performance improvement.

--- ULN2003_DRIVER.py
+++ ULN2003_DRIVER.py
@@ -9,51 +9,41 @@ class ULN2003:
         self.D = Pin(d, Pin.OUT)
 
         # Used to cycle through pins in clockwise and counterclockwise functions
-        self.pins = {
-            0 : self.A,
-            1 : self.B,
-            2 : self.C,
-            3 : self.D
-            4 : self.A
-            }
+        self.A_value = [1, 0, 0, 1]
+        self.B_value = [1, 1, 0, 0]
+        self.C_value = [0, 1, 1, 0]
+        self.D_value = [0, 0, 1, 1]
     
     def clockwise(self, steps, delay):
         if delay >= 0.002: # It won't move if it's below 0.002 (tested to 0.00199)
             for s in range(steps):
                 i = 0
-                while i < 3:
-                    self.pins[i].value(1)
-                    self.pins[i+1].value(1)
+                while i < 4:
+                    self.A.value(self.A_value[i])
+                    self.B.value(self.A_value[i])
+                    self.C.value(self.A_value[i])
+                    self.D.value(self.A_value[i])
                     sleep(delay)
-                    self.pins[i].value(0)
-                    self.pins[i+1].value(0)
                     i+=1
-                    sleep(delay)
         else:
-            i = 0
-            while i < 4:
-                self.pins[i].value(0)
-                i+=1
+            self.A.value(0)
+            self.B.value(0)
+            self.C.value(0)
+            self.D.value(0)
     
     def counterclockwise(self, steps, delay):
         if delay >= 0.002:
             for s in range(steps):
                 i = 3
-                self.D.value(1)
-                self.A.value(1)
-                sleep(delay)
-                self.D.value(0)
-                self.A.value(0)
-                while i > 0:
-                    self.pins[i].value(1)
-                    self.pins[i-1].value(1)
+                while i >= 0:
+                    self.A.value(self.A_value[i])
+                    self.B.value(self.A_value[i])
+                    self.C.value(self.A_value[i])
+                    self.D.value(self.A_value[i])
                     sleep(delay)
-                    self.pins[i].value(0)
-                    self.pins[i-1].value(0)
                     i-=1
-                    sleep(delay)
         else:
-            i = 0
-            while i < 4:
-                self.pins[i].value(0)
-                i+=1
+            self.A.value(0)
+            self.B.value(0)
+            self.C.value(0)
+            self.D.value(0)
