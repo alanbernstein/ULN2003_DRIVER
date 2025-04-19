335c4c35403b3aeb9ebeea935e7695ace91a97f0
Asher Bernstein-Meachum <asher.meachum@gmail.com>
2025-04-19 10:05:49 -0500

    Simplify functions for moving. Add input validation, error handling, and give more useful error messages.

--- ULN2003_DRIVER.py
+++ ULN2003_DRIVER.py
@@ -3,47 +3,41 @@ from time import sleep
 
 class ULN2003:
     def __init__(self, a, b, c, d):
-        self.A = Pin(a, Pin.OUT)
-        self.B = Pin(b, Pin.OUT)
-        self.C = Pin(c, Pin.OUT)
-        self.D = Pin(d, Pin.OUT)
+        try:
+            self.A = Pin(a, Pin.OUT)
+            self.B = Pin(b, Pin.OUT)
+            self.C = Pin(c, Pin.OUT)
+            self.D = Pin(d, Pin.OUT)
+        except ValueError:
+            print("Invalid pins provided. Check the pinout of your microcontroller.")
 
-        # Used to cycle through pins in clockwise and counterclockwise functions
+        # Used to cycle through the pin combinations in clockwise & counterclockwise.
         self.A_value = [1, 0, 0, 1]
         self.B_value = [1, 1, 0, 0]
         self.C_value = [0, 1, 1, 0]
         self.D_value = [0, 0, 1, 1]
     
-    def clockwise(self, steps, delay):
-        if delay >= 0.002: # It won't move if it's below 0.002 (tested to 0.00199)
-            for s in range(steps):
-                i = 0
-                while i < 4:
-                    self.A.value(self.A_value[i])
-                    self.B.value(self.A_value[i])
-                    self.C.value(self.A_value[i])
-                    self.D.value(self.A_value[i])
-                    sleep(delay)
-                    i+=1
+    def turn(self, direction: int, steps: int, delay: float = 0.002):
+        if delay >= 0.002 and steps > 0:
+            if direction == -1 or direction == 1:
+                for step in range(steps):
+                    i = 0
+                    for i in range(4):
+                        self.A.value(self.A_value[i*direction])
+                        self.B.value(self.B_value[i*direction])
+                        self.C.value(self.C_value[i*direction])
+                        self.D.value(self.D_value[i*direction])
+                        sleep(delay)
+                        i+=direction
+            else:
+                print("The value of direction is not valid. Please change the value to 1 (clockwise) or -1 (counterclockwise) to resolve this error.")
         else:
+            if delay < 0.002:
+                print("The value of delay is smaller than 0.002. Please increase the value of delay to resolve this error.")
+            if steps < 1:
+                print("The value of steps is smaller than 1. Please increase the value of steps to resolve this error.")
             self.A.value(0)
             self.B.value(0)
             self.C.value(0)
             self.D.value(0)
-    
-    def counterclockwise(self, steps, delay):
-        if delay >= 0.002:
-            for s in range(steps):
-                i = 3
-                while i >= 0:
-                    self.A.value(self.A_value[i])
-                    self.B.value(self.A_value[i])
-                    self.C.value(self.A_value[i])
-                    self.D.value(self.A_value[i])
-                    sleep(delay)
-                    i-=1
-        else:
-            self.A.value(0)
-            self.B.value(0)
-            self.C.value(0)
-            self.D.value(0)
+
