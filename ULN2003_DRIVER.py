9b89f17862190430d2932c31b7b66f2e3d864f23
Alan Bernstein <alan.aaron.bernstein@gmail.com>
2025-09-19 18:54:02 -0500

    Support multiple motors

--- ULN2003_DRIVER.py
+++ ULN2003_DRIVER.py
@@ -2,23 +2,23 @@ from machine import Pin
 from time import sleep
 
 class ULN2003:
-    def __init__(self, motor_pin_id_group):
+    def __init__(self, motor_pin_id_groups):
         try:
-            self.motor_pin_id_group = motor_pin_id_group
-            self.motor = [Pin(idx, Pin.OUT) for idx in self.motor_pin_id_group]
+            self.motor_pin_id_groups = motor_pin_id_groups
+            self.motors = [[Pin(idx, Pin.OUT) for idx in group] for group in self.motor_pin_id_group]
         except ValueError:
             print("Invalid pins provided. Check the pinout of your microcontroller.")
 
         # Used to cycle through the pin combinations in clockwise & counterclockwise.
         self.drive_values = [[1, 0, 0, 1], [1, 1, 0, 0], [0, 1, 1, 0], [0, 0, 1, 1]]
     
-    def turn(self, direction: int, steps: int, delay: float = 0.002):
+    def turn(self, motor_id: int, direction: int, steps: int, delay: float = 0.002):
         if delay >= 0.002 and steps > 0:
             if direction == -1 or direction == 1:
                 for step in range(steps):
                     i = 0
                     for i in range(4):
-                        [pin.value(drive) for pin, drive in zip(self.motor, self.drive_values)]
+                        [pin.value(drive) for pin, drive in zip(self.motors[motor_id], self.drive_values)]
                         sleep(delay)
                         i+=direction
             else:
@@ -28,4 +28,4 @@ class ULN2003:
                 print("The value of delay is smaller than 0.002. Please increase the value of delay to resolve this error.")
             if steps < 1:
                 print("The value of steps is smaller than 1. Please increase the value of steps to resolve this error.")
-            [pin.value(0) for pin in self.motor] # side effects inside a list comprehension is not the best form; may also not be supported by circuitpython
+            [pin.value(0) for pin in self.motors[motor_id]] # side effects inside a list comprehension is not the best form; may also not be supported by circuitpython
