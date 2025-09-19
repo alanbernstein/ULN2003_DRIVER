68516146858921ad6be3b9b312c2df5da5a2a3df
Alan Bernstein <alan.aaron.bernstein@gmail.com>
2025-09-19 18:52:03 -0500

    Operate on pins in list comprehensions

--- ULN2003_DRIVER.py
+++ ULN2003_DRIVER.py
@@ -5,20 +5,12 @@ class ULN2003:
     def __init__(self, motor_pin_id_group):
         try:
             self.motor_pin_id_group = motor_pin_id_group
-            self.motor = [
-                Pin(self.motor_pin_id_group[0], Pin.OUT),
-                Pin(self.motor_pin_id_group[1], Pin.OUT),
-                Pin(self.motor_pin_id_group[2], Pin.OUT),
-                Pin(self.motor_pin_id_group[3], Pin.OUT),
-            ]
+            self.motor = [Pin(idx, Pin.OUT) for idx in self.motor_pin_id_group]
         except ValueError:
             print("Invalid pins provided. Check the pinout of your microcontroller.")
 
         # Used to cycle through the pin combinations in clockwise & counterclockwise.
-        self.A_value = [1, 0, 0, 1]
-        self.B_value = [1, 1, 0, 0]
-        self.C_value = [0, 1, 1, 0]
-        self.D_value = [0, 0, 1, 1]
+        self.drive_values = [[1, 0, 0, 1], [1, 1, 0, 0], [0, 1, 1, 0], [0, 0, 1, 1]]
     
     def turn(self, direction: int, steps: int, delay: float = 0.002):
         if delay >= 0.002 and steps > 0:
@@ -26,10 +18,7 @@ class ULN2003:
                 for step in range(steps):
                     i = 0
                     for i in range(4):
-                        self.motor[0].value(self.A_value[i*direction])
-                        self.motor[1].value(self.B_value[i*direction])
-                        self.motor[2].value(self.C_value[i*direction])
-                        self.motor[3].value(self.D_value[i*direction])
+                        [pin.value(drive) for pin, drive in zip(self.motor, self.drive_values)]
                         sleep(delay)
                         i+=direction
             else:
@@ -39,7 +28,4 @@ class ULN2003:
                 print("The value of delay is smaller than 0.002. Please increase the value of delay to resolve this error.")
             if steps < 1:
                 print("The value of steps is smaller than 1. Please increase the value of steps to resolve this error.")
-            self.motor[0].value(0)
-            self.motor[1].value(0)
-            self.motor[2].value(0)
-            self.motor[3].value(0)
+            [pin.value(0) for pin in self.motor] # side effects inside a list comprehension is not the best form; may also not be supported by circuitpython
